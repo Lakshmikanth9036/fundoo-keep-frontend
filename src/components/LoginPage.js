b2a797cd04cd94c456b9c62dea3de2f2395f7943
Lakshmikanth9036 <kanth9036.1997@gmail.com>
2020-03-17 13:04:28 +0530

    [Lakshmikanth] added. added material ui and created registration page using material ui

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,7 +2,29 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import Constants from '../constants/Constants'
 import '../css/login.scss'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles';
+import { TextField, InputAdornment, InputLabel, FormControl, OutlinedInput, IconButton } from '@material-ui/core'
+import EmailIcon from '@material-ui/icons/Email';
+import ContactPhoneRoundedIcon from '@material-ui/icons/ContactPhoneRounded';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    margin: {
+      margin: theme.spacing(1),
+    },
+    withoutLabel: {
+      marginTop: theme.spacing(3),
+    },
+    textField: {
+      width: 200,
+    },
+  }));
 
 class LoginPage extends Component {
 
@@ -14,41 +36,95 @@ class LoginPage extends Component {
             mobile: '',
             password: ''
         }
+        this.showPassword =  false
     }
+
+    
 
     submitHandler = e => {
         e.preventDefault()
         console.log(this.state)
-        axios.put(Constants.loginApi,this.state)
-        .then(response => {
-            localStorage.setItem('response', response.data)
-            console.log(response)
-        }).catch(error => {
-            console.log('err')
-        })}
+        axios.put(Constants.loginApi, this.state)
+            .then(response => {
+                localStorage.setItem('response', response.data)
+                console.log(response)
+            }).catch(error => {
+                console.log('err')
+            })
     }
 
-    changeHandler = e => {
-        this.setState({ [e.target.name]: e.target.value })
-    }
-
-    render() {
-        const { emailAddress, mobile, password } = this.state
-        return (
-            <div>
-                <form onSubmit={this.submitHandler}>
-                    <div className='login'>
-                        <input type='text' name="emailAddress" value={emailAddress} onChange={this.changeHandler} placeholder="Email" />
-                        <h3>----------------Or---------------</h3>
-                        <input type='text' name="mobile" value={mobile} onChange={this.changeHandler} placeholder='Mobile No.' />
-                        <input type='password' name="password" value={password} onChange={this.changeHandler} placeholder='Password' />
-                        <button type='submit'>Login</button>
-                        <Link to="/" >Registration</Link>
-                    </div>
-                </form>
-            </div>
-        )
-    }
+changeHandler = e => {
+    this.setState({ [e.target.name]: e.target.value })
 }
 
+render() {
+    const { emailAddress, mobile, password } = this.state
+    return (
+        <div>
+            <form onSubmit={this.submitHandler}>
+                <div className='root'>
+                    {/* <input type='text' name="emailAddress" value={emailAddress} onChange={this.changeHandler} placeholder="Email" /> */}
+                    <TextField
+                            className="margin"
+                            name="emailAddress"
+                            value={emailAddress}
+                            onChange={this.changeHandler}
+                            label="Email"
+                            variant="outlined"
+                            size="small"
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <EmailIcon />
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                    <h3>----------------Or---------------</h3>
+                    {/* <input type='text' name="mobile" value={mobile} onChange={this.changeHandler} placeholder='Mobile No.' /> */}
+                    <TextField
+                            className="margin"
+                            name="mobile"
+                            value={mobile}
+                            onChange={this.changeHandler}
+                            label="MobileNo."
+                            variant="outlined"
+                            size="small"
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <ContactPhoneRoundedIcon />
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                    {/* <input type='password' name="password" value={password} onChange={this.changeHandler} placeholder='Password' /> */}
+                    {/* <FormControl variant="outlined" size="small">
+                            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-password"
+                                type={showPassword ? 'text' : 'password'}
+                                value={fields.password}
+                                onChange={this.changeHandler}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={this.handleClickShowPassword}
+                                            edge="end">
+                                            {showPassword ? <Visibility /> : <VisibilityOff />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                />
+                        </FormControl> */}
+                    <button type='submit'>Login</button>
+                    <Link to="/" >Registration</Link>
+                </div>
+            </form>
+        </div>
+    );
+
+    }
+}
 export default LoginPage
--- src/components/RegistrationPage.js
+++ src/components/RegistrationPage.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react'
 import RegistrationService from '../service/RegistrationService'
 import '../css/register.scss'
 import axios from 'axios'
+import clsx from 'clsx'
 import Constants from '../constants/Constants'
 import LoginPage from './LoginPage'
 import { Link } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Input from '@material-ui/core/Input';
+import IconButton from '@material-ui/core/IconButton';
+import InputLabel from '@material-ui/core/InputLabel';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import { FilledInput } from '@material-ui/core'
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import EmailIcon from '@material-ui/icons/Email';
+import ContactPhoneRoundedIcon from '@material-ui/icons/ContactPhoneRounded';
+import Button from '@material-ui/core/Button';
 
 class RegistrationPage extends Component {
 
@@ -13,13 +30,30 @@ class RegistrationPage extends Component {
 
         this.state = {
             fields: {},
-            errors: {}
+            errors: {},
+            showPassword: false
         }
 
         this.changeHandler = this.changeHandler.bind(this)
         this.submitHandler = this.submitHandler.bind(this)
     }
 
+    useStyles = makeStyles(theme => ({
+        root: {
+            display: 'flex',
+            flexWrap: 'wrap',
+        },
+        margin: {
+            margin: theme.spacing(1),
+        },
+        withoutLabel: {
+            marginTop: theme.spacing(3),
+        },
+        textField: {
+            width: 200,
+        },
+    }));
+ 
     changeHandler = (e) => {
         let fields = this.state.fields
         fields[e.target.name] = e.target.value
@@ -45,6 +79,12 @@ class RegistrationPage extends Component {
                     console.log(error)
                 })
         }
+    }
+
+    handleClickShowPassword() {
+        console.log(this.state.showPassword)
+        let showPassword = !this.state.showPassword
+        this.setState({ showPassword })
     }
 
     isValidForm() {
@@ -122,21 +162,106 @@ class RegistrationPage extends Component {
 
     render() {
         const { fields, errors } = this.state
+        const { showPassword } = this.state
         return (
             <div>
                 <form method="post" onSubmit={this.submitHandler}>
                     <div className='root'>
-                        <input type='text' name="firstName" value={fields.firstName} onChange={this.changeHandler} placeholder='First Name' />
+                        {/* <input type='text' name="firstName" value={fields.firstName} onChange={this.changeHandler} placeholder='First Name' /> */}
+                        <TextField
+                            className={this.useStyles.margin}
+                            name="firstName"
+                            value={fields.firstName}
+                            onChange={this.changeHandler}
+                            label="FirstName"
+                            variant="outlined"
+                            size="small"
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <AccountCircle />
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
                         <div className="errorMsg">{errors.firstName}</div>
-                        <input type='text' name="lastName" value={fields.lastName} onChange={this.changeHandler} placeholder='Last Name' />
+                        {/* <input type='text' name="lastName" value={fields.lastName} onChange={this.changeHandler} placeholder='Last Name' /> */}
+                        <TextField
+                            className={this.useStyles.margin}
+                            name="lastName"
+                            value={fields.lastName}
+                            onChange={this.changeHandler}
+                            label="LastName"
+                            variant="outlined"
+                            size="small"
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <AccountCircle />
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
                         <div className="errorMsg">{errors.lastName}</div>
-                        <input type='text' name="emailAddress" value={fields.emailAddress} onChange={this.changeHandler} placeholder="Email" />
+                        {/* <input type='text' name="emailAddress" value={fields.emailAddress} onChange={this.changeHandler} placeholder="Email" /> */}
+                        <TextField
+                            className={this.useStyles.margin}
+                            name="emailAddress"
+                            value={fields.emailAddress}
+                            onChange={this.changeHandler}
+                            label="Email"
+                            variant="outlined"
+                            size="small"
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <EmailIcon />
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
                         <div className="errorMsg">{errors.emailAddress}</div>
-                        <input type='text' name="mobile" value={fields.mobile} onChange={this.changeHandler} placeholder='Mobile No.' />
+                        {/* <input type='text' name="mobile" value={fields.mobile} onChange={this.changeHandler} placeholder='Mobile No.' /> */}
+                        <TextField
+                            className={this.useStyles.margin}
+                            name="mobile"
+                            value={fields.mobile}
+                            onChange={this.changeHandler}
+                            label="MobileNo."
+                            variant="outlined"
+                            size="small"
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <ContactPhoneRoundedIcon />
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
                         <div className="errorMsg">{errors.mobile}</div>
-                        <input type='password' name="password" value={fields.password} onChange={this.changeHandler} placeholder='Password' />
+                        {/* <input type='password' name="password" value={fields.password} onChange={this.changeHandler} placeholder='Password' /> */}
+                        <FormControl variant="outlined" size="small">
+                            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-password"
+                                type={showPassword ? 'text' : 'password'}
+                                value={fields.password}
+                                onChange={this.changeHandler}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={this.handleClickShowPassword}
+                                            edge="end">
+                                            {showPassword ? <Visibility /> : <VisibilityOff />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                />
+                        </FormControl>
                         <div className="errorMsg">{errors.password}</div>
-                        <button type='submit' >Submit</button>
+                        {/* <button type='submit' >Submit</button> */}
+                        <Button className='btn' type='submit' variant="contained" color="primary">Signup</Button><br/>
                         <Link to="/login" >Login</Link>
                     </div>
                 </form>
@@ -148,139 +273,3 @@ class RegistrationPage extends Component {
 export default RegistrationPage
 
 
-// import React from 'react';
-
-
-
-// class RegistrationPage extends React.Component {
-//     constructor() {
-//       super();
-//       this.state = {
-//         fields: {},
-//         errors: {}
-//       }
-
-//       this.handleChange = this.handleChange.bind(this);
-//       this.submituserRegistrationForm = this.submituserRegistrationForm.bind(this);
-
-//     };
-
-//     handleChange(e) {
-//       let fields = this.state.fields;
-//       fields[e.target.name] = e.target.value;
-//       this.setState({
-//         fields
-//       });
-
-//     }
-
-//     submituserRegistrationForm(e) {
-//       e.preventDefault();
-//       if (this.validateForm()) {
-//           let fields = {};
-//           fields["username"] = "";
-//           fields["emailid"] = "";
-//           fields["mobileno"] = "";
-//           fields["password"] = "";
-//           this.setState({fields:fields});
-//           console.log('hello',this.state.fields)
-//           alert("Form submitted");
-//       }
-
-//     }
-
-//     validateForm() {
-
-//       let fields = this.state.fields;
-//       let errors = {};
-//       let formIsValid = true;
-
-//       if (!fields["username"]) {
-//         formIsValid = false;
-//         errors["username"] = "*Please enter your username.";
-//       }
-
-//       if (typeof fields["username"] !== "undefined") {
-//         if (!fields["username"].match(/^[a-zA-Z ]*$/)) {
-//           formIsValid = false;
-//           errors["username"] = "*Please enter alphabet characters only.";
-//         }
-//       }
-
-//       if (!fields["emailid"]) {
-//         formIsValid = false;
-//         errors["emailid"] = "*Please enter your email-ID.";
-//       }
-
-//       if (typeof fields["emailid"] !== "undefined") {
-//         //regular expression for email validation
-//         var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-//         if (!pattern.test(fields["emailid"])) {
-//           formIsValid = false;
-//           errors["emailid"] = "*Please enter valid email-ID.";
-//         }
-//       }
-
-//       if (!fields["mobileno"]) {
-//         formIsValid = false;
-//         errors["mobileno"] = "*Please enter your mobile no.";
-//       }
-
-//       if (typeof fields["mobileno"] !== "undefined") {
-//         if (!fields["mobileno"].match(/^[0-9]{10}$/)) {
-//           formIsValid = false;
-//           errors["mobileno"] = "*Please enter valid mobile no.";
-//         }
-//       }
-
-//       if (!fields["password"]) {
-//         formIsValid = false;
-//         errors["password"] = "*Please enter your password.";
-//       }
-
-//       if (typeof fields["password"] !== "undefined") {
-//         if (!fields["password"].match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)) {
-//           formIsValid = false;
-//           errors["password"] = "*Please enter secure and strong password.";
-//         }
-//       }
-
-//       this.setState({
-//         errors: errors
-//       });
-//       return formIsValid;
-
-
-//     }
-
-//   render() {
-//     return (
-//     <div id="main-registration-container">
-//      <div id="register">
-//         <h3>Registration page</h3>
-//         <form method="post"  name="userRegistrationForm"  onSubmit= {this.submituserRegistrationForm} >
-//         <label>Name</label>
-//         <input type="text" name="username" value={this.state.fields.username} onChange={this.handleChange} />
-//         <div className="errorMsg">{this.state.errors.username}</div>
-//         <label>Email ID:</label>
-//         <input type="text" name="emailid" value={this.state.fields.emailid} onChange={this.handleChange}  />
-//         <div className="errorMsg">{this.state.errors.emailid}</div>
-//         <label>Mobile No:</label>
-//         <input type="text" name="mobileno" value={this.state.fields.mobileno} onChange={this.handleChange}   />
-//         <div className="errorMsg">{this.state.errors.mobileno}</div>
-//         <label>Password</label>
-//         <input type="password" name="password" value={this.state.fields.password} onChange={this.handleChange} />
-//         <div className="errorMsg">{this.state.errors.password}</div>
-//         <input type="submit" className="button"  value="Register"/>
-//         </form>
-//     </div>
-// </div>
-
-//       );
-//   }
-
-
-// }
-
-
-// export default RegistrationPage;
